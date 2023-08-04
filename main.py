3bb7e4b1ad294ef644bf787dfc9c60464aec6576
Nguyen Mau Duy <duynm07@gmail.com>
2023-08-04 18:15:23 +0700

    add print debug

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ def read_root():
 @app.post("https://hooks.zapier.com/hooks/catch/16132664/31x07ar/")
 def webhook(payload: dict):
     # Handle the payload from Zapier
+    print("Received data:", payload)
     return {"message": "Webhook received"}
