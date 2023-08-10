60769cb69c10476a0697a3a3873012edf4117661
Nguyen Mau Duy <duynm@Nguyens-MacBook-Pro.local>
2023-08-10 11:26:59 +0700

    update main

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
+from io import BytesIO
+from typing import Optional
+
+from fastapi import APIRouter, File, UploadFile, FastAPI
+from fastapi.responses import JSONResponse
+from PIL import Image
+from starlette import status
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/test", status_code=status.HTTP_200_OK)
+async def ocr(file: UploadFile = File(...)):
+    try:
+        file_data = file.read()
+        print(file_data)
+        # image_data = await file.read()
+        # image = Image.open(BytesIO(image_data)).convert("RGB")
 
-@app.post("https://hooks.zapier.com/hooks/catch/16132664/31ph42a/")
-def webhook(payload: dict):
-    # Handle the payload from Zapier
-    print("Received data:", payload)
-    return {"message": "Webhook received"}
+        return JSONResponse(content={"message": "Success"})
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
